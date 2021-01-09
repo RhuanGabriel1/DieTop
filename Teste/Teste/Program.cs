ccb3d5dcd2016cfb90e67f52730e7c0723ea56fc
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-01-09 15:10:13 -0300

    Update Program.cs

--- Teste/Teste/Program.cs
+++ Teste/Teste/Program.cs
@@ -10,7 +10,7 @@ namespace Teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
