899b741cabc460ce3f4f7a3a7889d71c901941a8
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-01-10 01:12:47 -0300

    START GUI PROJECT AND CREATE/PERSONALIZE A WELCOME VIEW

---
+++ DieTop/Views/Welcome_View.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace DieTop
+{
+    partial class Welcome_View
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeText
+            // 
+            this.welcomeText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.welcomeText.AutoSize = true;
+            this.welcomeText.Font = new System.Drawing.Font("Microsoft PhagsPa", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeText.ForeColor = System.Drawing.Color.Black;
+            this.welcomeText.Location = new System.Drawing.Point(242, 50);
+            this.welcomeText.Name = "welcomeText";
+            this.welcomeText.Size = new System.Drawing.Size(492, 45);
+            this.welcomeText.TabIndex = 0;
+            this.welcomeText.Text = "Sejam Bem-Vindos ao DieTop!";
+            this.welcomeText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Welcome_View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1000, 700);
+            this.Controls.Add(this.welcomeText);
+            this.Name = "Welcome_View";
+            this.Text = "Die Top Program!";
+            this.Load += new System.EventHandler(this.Welcome_View_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeText;
+    }
+}
+
