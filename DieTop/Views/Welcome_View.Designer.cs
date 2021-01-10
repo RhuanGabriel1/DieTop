b424249ad8197e96a4589d84e3e203a85cb05f2e
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-01-10 13:02:13 -0300

    Insert some Data, need attencion of image bug

--- DieTop/Views/Welcome_View.Designer.cs
+++ DieTop/Views/Welcome_View.Designer.cs
@@ -29,7 +29,15 @@ namespace DieTop
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome_View));
             this.welcomeText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputLogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputSenha = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.newRegister = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // welcomeText
@@ -37,21 +45,134 @@ namespace DieTop
             this.welcomeText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.welcomeText.AutoSize = true;
-            this.welcomeText.Font = new System.Drawing.Font("Microsoft PhagsPa", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.welcomeText.ForeColor = System.Drawing.Color.Black;
-            this.welcomeText.Location = new System.Drawing.Point(242, 50);
+            this.welcomeText.BackColor = System.Drawing.Color.Transparent;
+            this.welcomeText.Cursor = System.Windows.Forms.Cursors.Default;
+            this.welcomeText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.welcomeText.Font = new System.Drawing.Font("Microsoft PhagsPa", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeText.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.welcomeText.Location = new System.Drawing.Point(201, 48);
             this.welcomeText.Name = "welcomeText";
-            this.welcomeText.Size = new System.Drawing.Size(492, 45);
+            this.welcomeText.Size = new System.Drawing.Size(552, 49);
             this.welcomeText.TabIndex = 0;
             this.welcomeText.Text = "Sejam Bem-Vindos ao DieTop!";
             this.welcomeText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.welcomeText.Click += new System.EventHandler(this.welcomeText_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(277, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Faça o Login para Continuar!";
+            // 
+            // inputLogin
+            // 
+            this.inputLogin.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.inputLogin.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLogin.ForeColor = System.Drawing.Color.Silver;
+            this.inputLogin.Location = new System.Drawing.Point(236, 223);
+            this.inputLogin.Multiline = true;
+            this.inputLogin.Name = "inputLogin";
+            this.inputLogin.Size = new System.Drawing.Size(433, 31);
+            this.inputLogin.TabIndex = 2;
+            this.inputLogin.Tag = "";
+            this.inputLogin.Text = "Insira seu Login";
+            this.inputLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputLogin.WordWrap = false;
+            this.inputLogin.TextChanged += new System.EventHandler(this.loginInput_TextChanged);
+            this.inputLogin.Enter += new System.EventHandler(this.inputLogin_Enter);
+            this.inputLogin.Leave += new System.EventHandler(this.inputLogin_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(150, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Login";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(141, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Senha";
+            // 
+            // inputSenha
+            // 
+            this.inputSenha.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputSenha.ForeColor = System.Drawing.Color.Silver;
+            this.inputSenha.Location = new System.Drawing.Point(236, 303);
+            this.inputSenha.Multiline = true;
+            this.inputSenha.Name = "inputSenha";
+            this.inputSenha.Size = new System.Drawing.Size(433, 31);
+            this.inputSenha.TabIndex = 6;
+            this.inputSenha.Tag = "";
+            this.inputSenha.Text = "Insira sua Senha";
+            this.inputSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputSenha.WordWrap = false;
+            this.inputSenha.TextChanged += new System.EventHandler(this.loginInput_TextChanged);
+            this.inputSenha.Enter += new System.EventHandler(this.inputSenha_enter);
+            this.inputSenha.Leave += new System.EventHandler(this.inputSenha_leave);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.Window;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.loginButton.Location = new System.Drawing.Point(310, 374);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(252, 47);
+            this.loginButton.TabIndex = 7;
+            this.loginButton.Text = "Logar";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // newRegister
+            // 
+            this.newRegister.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.newRegister.AutoSize = true;
+            this.newRegister.BackColor = System.Drawing.Color.Transparent;
+            this.newRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.newRegister.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newRegister.LinkColor = System.Drawing.Color.Black;
+            this.newRegister.Location = new System.Drawing.Point(290, 451);
+            this.newRegister.Name = "newRegister";
+            this.newRegister.Size = new System.Drawing.Size(296, 28);
+            this.newRegister.TabIndex = 8;
+            this.newRegister.TabStop = true;
+            this.newRegister.Text = "Ainda não tenho Cadastro!";
             // 
             // Welcome_View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(1000, 700);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(956, 574);
+            this.Controls.Add(this.newRegister);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.inputSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputLogin);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.welcomeText);
             this.Name = "Welcome_View";
             this.Text = "Die Top Program!";
@@ -64,6 +185,13 @@ namespace DieTop
         #endregion
 
         private System.Windows.Forms.Label welcomeText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputSenha;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.LinkLabel newRegister;
     }
 }
 
--- DieTop/Views/Welcome_View.cs
+++ DieTop/Views/Welcome_View.cs
@@ -10,11 +10,34 @@ using System.Windows.Forms;
 
 namespace DieTop
 {
+  
+
     public partial class Welcome_View : Form
     {
         public Welcome_View()
         {
             InitializeComponent();
+        }
+        //If clicked is different of default text , will turn of personalized text
+
+        protected void changeInputTextClicked(TextBox textBox, String text)
+        {
+            if (textBox.Text == text)
+            {
+                textBox.Text = "";
+                textBox.ForeColor = Color.Black;
+                textBox.TextAlign = HorizontalAlignment.Left;
+            }
+        }
+        //If clicked button text === "", will turn all for default
+        protected void changeInputTextClickedNull(TextBox textBox, String text)
+        {
+            if (textBox.Text == "")
+            {
+                textBox.Text = text;
+                textBox.ForeColor = Color.Silver;
+                textBox.TextAlign = HorizontalAlignment.Center;
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -31,5 +54,48 @@ namespace DieTop
         {
 
         }
+
+        private void welcomeText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void inputSenha_enter(object sender, EventArgs e)
+        {
+            changeInputTextClicked(inputSenha, "Insira sua Senha");
+
+        }
+        private void inputSenha_leave(object sender, EventArgs e)
+        {
+            changeInputTextClickedNull(inputSenha, "Insira sua Senha");
+
+        }
+        private void inputLogin_Enter(object sender, EventArgs e)
+        {
+            changeInputTextClicked(inputLogin, "Insira seu Login");
+
+        }
+        private void inputLogin_Leave(object sender, EventArgs e)
+        {
+            changeInputTextClickedNull(inputLogin, "Insira seu Login");
+
+        }
+        private void inputSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
 }
