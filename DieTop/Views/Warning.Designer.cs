e05819344d81f7682c4a6b9b0ddbc99680fcd844
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-01-29 13:28:48 -0300

    Design of Warning is complete

--- DieTop/Views/Warning.Designer.cs
+++ DieTop/Views/Warning.Designer.cs
@@ -37,6 +37,8 @@ namespace DieTop
             this.labelAvisoLinha4 = new System.Windows.Forms.Label();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +46,7 @@ namespace DieTop
             // 
             this.labelAvisoLinha1.AutoSize = true;
             this.labelAvisoLinha1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAvisoLinha1.Location = new System.Drawing.Point(205, 235);
+            this.labelAvisoLinha1.Location = new System.Drawing.Point(212, 204);
             this.labelAvisoLinha1.Name = "labelAvisoLinha1";
             this.labelAvisoLinha1.Size = new System.Drawing.Size(343, 20);
             this.labelAvisoLinha1.TabIndex = 0;
@@ -54,7 +56,7 @@ namespace DieTop
             // 
             this.labelAvisloLinha2.AutoSize = true;
             this.labelAvisloLinha2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAvisloLinha2.Location = new System.Drawing.Point(174, 267);
+            this.labelAvisloLinha2.Location = new System.Drawing.Point(181, 236);
             this.labelAvisloLinha2.Name = "labelAvisloLinha2";
             this.labelAvisloLinha2.Size = new System.Drawing.Size(413, 20);
             this.labelAvisloLinha2.TabIndex = 1;
@@ -64,7 +66,7 @@ namespace DieTop
             // 
             this.labelAvisoLinha3.AutoSize = true;
             this.labelAvisoLinha3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAvisoLinha3.Location = new System.Drawing.Point(141, 299);
+            this.labelAvisoLinha3.Location = new System.Drawing.Point(148, 268);
             this.labelAvisoLinha3.Name = "labelAvisoLinha3";
             this.labelAvisoLinha3.Size = new System.Drawing.Size(485, 20);
             this.labelAvisoLinha3.TabIndex = 2;
@@ -74,7 +76,7 @@ namespace DieTop
             // 
             this.labelAvisoLinha4.AutoSize = true;
             this.labelAvisoLinha4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAvisoLinha4.Location = new System.Drawing.Point(58, 328);
+            this.labelAvisoLinha4.Location = new System.Drawing.Point(65, 297);
             this.labelAvisoLinha4.Name = "labelAvisoLinha4";
             this.labelAvisoLinha4.Size = new System.Drawing.Size(677, 20);
             this.labelAvisoLinha4.TabIndex = 3;
@@ -89,24 +91,56 @@ namespace DieTop
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(209, 65);
+            this.pictureBox1.Location = new System.Drawing.Point(185, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(345, 142);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.Size = new System.Drawing.Size(409, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.DarkRed;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.loginButton.Location = new System.Drawing.Point(185, 359);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(192, 47);
+            this.loginButton.TabIndex = 8;
+            this.loginButton.Text = "Voltar";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(402, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Aceitar";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Warning
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loginButton);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelAvisoLinha4);
             this.Controls.Add(this.labelAvisoLinha3);
             this.Controls.Add(this.labelAvisloLinha2);
             this.Controls.Add(this.labelAvisoLinha1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Warning";
             this.Text = "Warning";
             this.Load += new System.EventHandler(this.Warning_Load);
@@ -124,5 +158,7 @@ namespace DieTop
         private System.Windows.Forms.Label labelAvisoLinha4;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button button1;
     }
 }
--- DieTop/Views/Warning.cs
+++ DieTop/Views/Warning.cs
@@ -21,5 +21,10 @@ namespace DieTop
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
