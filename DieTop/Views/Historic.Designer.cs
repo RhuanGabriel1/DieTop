cea2e0b707e47203625de8797af2893674d014b0
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-02-01 19:10:43 -0300

    Design for Historic is complete

--- DieTop/Views/Historic.Designer.cs
+++ DieTop/Views/Historic.Designer.cs
@@ -32,6 +32,29 @@ namespace DieTop.Views
             this.ButtonVoltar = new System.Windows.Forms.Button();
             this.selectDay = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelAtvSem = new System.Windows.Forms.Label();
+            this.labelDuracao = new System.Windows.Forms.Label();
+            this.labelDieta = new System.Windows.Forms.Label();
+            this.labelAlmoco = new System.Windows.Forms.Label();
+            this.labelJanta = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelRestante = new System.Windows.Forms.Label();
+            this.labelAtividade = new System.Windows.Forms.Label();
+            this.labelCafe = new System.Windows.Forms.Label();
+            this.labelDia = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonAtt = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ButtonVoltar
@@ -72,18 +95,274 @@ namespace DieTop.Views
             this.label9.TabIndex = 42;
             this.label9.Text = "Selecione o Dia que Deseja Consultar";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.labelAtvSem);
+            this.panel1.Controls.Add(this.labelDuracao);
+            this.panel1.Controls.Add(this.labelDieta);
+            this.panel1.Controls.Add(this.labelAlmoco);
+            this.panel1.Controls.Add(this.labelJanta);
+            this.panel1.Controls.Add(this.labelTotal);
+            this.panel1.Controls.Add(this.labelRestante);
+            this.panel1.Controls.Add(this.labelAtividade);
+            this.panel1.Controls.Add(this.labelCafe);
+            this.panel1.Controls.Add(this.labelDia);
+            this.panel1.Controls.Add(this.label16);
+            this.panel1.Controls.Add(this.label15);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(36, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(855, 401);
+            this.panel1.TabIndex = 44;
+            // 
+            // labelAtvSem
+            // 
+            this.labelAtvSem.AutoSize = true;
+            this.labelAtvSem.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAtvSem.Location = new System.Drawing.Point(530, 119);
+            this.labelAtvSem.Name = "labelAtvSem";
+            this.labelAtvSem.Size = new System.Drawing.Size(19, 21);
+            this.labelAtvSem.TabIndex = 53;
+            this.labelAtvSem.Text = "0";
+            this.labelAtvSem.Visible = false;
+            // 
+            // labelDuracao
+            // 
+            this.labelDuracao.AutoSize = true;
+            this.labelDuracao.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDuracao.Location = new System.Drawing.Point(530, 64);
+            this.labelDuracao.Name = "labelDuracao";
+            this.labelDuracao.Size = new System.Drawing.Size(19, 21);
+            this.labelDuracao.TabIndex = 52;
+            this.labelDuracao.Text = "0";
+            this.labelDuracao.Visible = false;
+            // 
+            // labelDieta
+            // 
+            this.labelDieta.AutoSize = true;
+            this.labelDieta.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDieta.Location = new System.Drawing.Point(190, 309);
+            this.labelDieta.Name = "labelDieta";
+            this.labelDieta.Size = new System.Drawing.Size(19, 21);
+            this.labelDieta.TabIndex = 51;
+            this.labelDieta.Text = "0";
+            this.labelDieta.Visible = false;
+            // 
+            // labelAlmoco
+            // 
+            this.labelAlmoco.AutoSize = true;
+            this.labelAlmoco.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAlmoco.Location = new System.Drawing.Point(190, 101);
+            this.labelAlmoco.Name = "labelAlmoco";
+            this.labelAlmoco.Size = new System.Drawing.Size(19, 21);
+            this.labelAlmoco.TabIndex = 50;
+            this.labelAlmoco.Text = "0";
+            this.labelAlmoco.Visible = false;
+            // 
+            // labelJanta
+            // 
+            this.labelJanta.AutoSize = true;
+            this.labelJanta.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelJanta.Location = new System.Drawing.Point(190, 140);
+            this.labelJanta.Name = "labelJanta";
+            this.labelJanta.Size = new System.Drawing.Size(19, 21);
+            this.labelJanta.TabIndex = 49;
+            this.labelJanta.Text = "0";
+            this.labelJanta.Visible = false;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(190, 181);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(19, 21);
+            this.labelTotal.TabIndex = 48;
+            this.labelTotal.Text = "0";
+            this.labelTotal.Visible = false;
+            // 
+            // labelRestante
+            // 
+            this.labelRestante.AutoSize = true;
+            this.labelRestante.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRestante.Location = new System.Drawing.Point(190, 220);
+            this.labelRestante.Name = "labelRestante";
+            this.labelRestante.Size = new System.Drawing.Size(19, 21);
+            this.labelRestante.TabIndex = 47;
+            this.labelRestante.Text = "0";
+            this.labelRestante.Visible = false;
+            // 
+            // labelAtividade
+            // 
+            this.labelAtividade.AutoSize = true;
+            this.labelAtividade.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAtividade.Location = new System.Drawing.Point(190, 263);
+            this.labelAtividade.Name = "labelAtividade";
+            this.labelAtividade.Size = new System.Drawing.Size(19, 21);
+            this.labelAtividade.TabIndex = 46;
+            this.labelAtividade.Text = "0";
+            this.labelAtividade.Visible = false;
+            // 
+            // labelCafe
+            // 
+            this.labelCafe.AutoSize = true;
+            this.labelCafe.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCafe.Location = new System.Drawing.Point(190, 64);
+            this.labelCafe.Name = "labelCafe";
+            this.labelCafe.Size = new System.Drawing.Size(19, 21);
+            this.labelCafe.TabIndex = 45;
+            this.labelCafe.Text = "0";
+            this.labelCafe.Visible = false;
+            // 
+            // labelDia
+            // 
+            this.labelDia.AutoSize = true;
+            this.labelDia.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDia.Location = new System.Drawing.Point(86, 23);
+            this.labelDia.Name = "labelDia";
+            this.labelDia.Size = new System.Drawing.Size(24, 28);
+            this.labelDia.TabIndex = 44;
+            this.labelDia.Text = "0";
+            this.labelDia.Visible = false;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(20, 23);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(60, 28);
+            this.label16.TabIndex = 43;
+            this.label16.Text = "DIA :";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(328, 119);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(174, 21);
+            this.label15.TabIndex = 42;
+            this.label15.Text = "Atividades Semanais :";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(21, 309);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(114, 21);
+            this.label14.TabIndex = 41;
+            this.label14.Text = "Dieta do Dia :";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(328, 64);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(183, 21);
+            this.label13.TabIndex = 40;
+            this.label13.Text = "Duração da Atividade :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(21, 263);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(152, 21);
+            this.label12.TabIndex = 39;
+            this.label12.Text = "Tipo de Atívidade :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(21, 220);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(157, 21);
+            this.label11.TabIndex = 38;
+            this.label11.Text = "Calorias Restantes :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(21, 181);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 21);
+            this.label10.TabIndex = 37;
+            this.label10.Text = "Total de Calorias :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Almoço :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(21, 140);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 21);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "Janta :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 21);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "Café da Manhã :";
+            // 
+            // buttonAtt
+            // 
+            this.buttonAtt.BackColor = System.Drawing.SystemColors.Window;
+            this.buttonAtt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonAtt.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAtt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonAtt.Location = new System.Drawing.Point(571, 43);
+            this.buttonAtt.Name = "buttonAtt";
+            this.buttonAtt.Size = new System.Drawing.Size(91, 28);
+            this.buttonAtt.TabIndex = 45;
+            this.buttonAtt.Text = "Atualizar";
+            this.buttonAtt.UseVisualStyleBackColor = false;
+            // 
             // Historic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(956, 574);
+            this.Controls.Add(this.buttonAtt);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.selectDay);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.ButtonVoltar);
             this.Name = "Historic";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Refeição!";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,5 +372,27 @@ namespace DieTop.Views
         private System.Windows.Forms.Button ButtonVoltar;
         private System.Windows.Forms.ComboBox selectDay;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelAtvSem;
+        private System.Windows.Forms.Label labelDuracao;
+        private System.Windows.Forms.Label labelDieta;
+        private System.Windows.Forms.Label labelAlmoco;
+        private System.Windows.Forms.Label labelJanta;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelRestante;
+        private System.Windows.Forms.Label labelAtividade;
+        private System.Windows.Forms.Label labelCafe;
+        private System.Windows.Forms.Label labelDia;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonAtt;
     }
 }
